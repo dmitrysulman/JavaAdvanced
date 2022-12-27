b1d71228ae0185613715f036cb7fa42d552a5378
Dmitry Sulman <dmitry.sulman@gmail.com>
2022-12-27 13:32:09 +0200

    overlap

--- src/main/java/other/FindOverlapChars.java
+++ src/main/java/other/FindOverlapChars.java
@@ -1,26 +1,34 @@
 package other;
 
+import java.util.Scanner;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class FindOverlapChars {
     public static void main(String[] args) {
-        Set<Character> overlap = findOverlap("abcd", "daqwe", "podla");
-        overlap.forEach(System.out::println);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of strings: ");
+        int n = Integer.parseInt(scanner.nextLine());
+        String[] strings = new String[n];
+        for (int i = 0; i < n; i++) {
+            System.out.printf("Enter string #%d: ", i);
+            strings[i] = scanner.nextLine();
+        }
+        Set<Character> overlap = findOverlap(strings);
+        overlap.forEach(System.out::print);
     }
 
-    private static Set<Character> findOverlap(String s1, String s2, String s3) {
-        Set<Integer> set1 = s1.chars()
-                .boxed()
+    private static Set<Character> findOverlap(String... strings) {
+        Set<Character> result = getSetOfCharsFromString(strings[0]);
+        for (int i = 1; i < strings.length; i++) {
+            result.retainAll(getSetOfCharsFromString(strings[i]));
+        }
+        return result;
+    }
+
+    private static Set<Character> getSetOfCharsFromString(String string) {
+        return string.chars()
+                .mapToObj(e -> (char) e)
                 .collect(Collectors.toSet());
-        Set<Integer> set2 = s2.chars()
-                .boxed()
-                .collect(Collectors.toSet());
-        Set<Integer> set3 = s3.chars()
-                .boxed()
-                .collect(Collectors.toSet());
-        set1.retainAll(set2);
-        set1.retainAll(set3);
-        return set1.stream().map(e -> (char) e.intValue()).collect(Collectors.toSet());
     }
 }
